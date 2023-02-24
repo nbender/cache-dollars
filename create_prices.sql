8c7f3b3f36fbee7544a5b7b0a4bddf4317c6f8b5
Nick Bender <nbender@users.noreply.github.com>
2023-02-24 21:55:41 +0000

    Update create_prices.sql

--- create_prices.sql
+++ create_prices.sql
@@ -2,7 +2,7 @@ drop table if exists prices;
 
 with staged as (
      select convert_to_date(date) as date, symbol, open, high, low, close, volume, file_date
-     from stage_prices
+     from stage_prices where update = false
 )
 select *
 into prices
@@ -11,4 +11,3 @@ where date is not null;
 
 create index prices_by__date__symbol on prices (date, symbol);
 create index prices_by__symbol__date on prices (symbol, date);
-
