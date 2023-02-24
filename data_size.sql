fe05bb83c9ca8208688501dfdf301a01b6eaa607
Nick Bender <nbender@users.noreply.github.com>
2023-02-24 21:43:09 +0000

    Create data_size.sql

---
+++ data_size.sql
@@ -0,0 +1,7 @@
+
+select table_name
+     , pg_size_pretty(pg_relation_size(quote_ident(table_name)))
+     , pg_relation_size(quote_ident(table_name))
+from information_schema.tables
+where table_schema = 'public'
+order by 3 desc;
