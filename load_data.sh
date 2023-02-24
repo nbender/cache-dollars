509e9f5ba5a8ab918857f09bdef3f0291a306a9d
Nick Bender <nbender@users.noreply.github.com>
2023-02-24 21:40:56 +0000

    Update load_data.sh

--- load_data.sh
+++ load_data.sh
@@ -1,4 +1,21 @@
-echo ~/git/pystock-data/2015/20150324.tar.gz
-tar xvfzO ~/git/pystock-data/2015/20150324.tar.gz prices.csv | psql -c "copy stage_prices from stdin with (format csv, header)"
-tar xvfzO ~/git/pystock-data/2015/20150324.tar.gz symbols.txt | psql -c "copy stage_symbols (symbol, name) from stdin with (format text); update stage_symbols set date='20150324' where date is null"
+set -e
 
+psql -f create_stage_tables.sql
+
+for f in ~/git/pystock-data/????/????????.tar.gz; do
+  d=`basename $f .tar.gz`
+
+  echo "tar xvfzO $f prices.csv"
+  tar xvfzO $f prices.csv | psql -c "copy stage_prices (symbol, date, open, high, low, close, volume, adj_close) from stdin with (format csv, header)"
+  echo "tar xvfzO $f symbols.txt"
+  tar xvfzO $f symbols.txt | psql -c "copy stage_symbols (symbol, name) from stdin with (format text)"
+
+  echo "update load information"
+  psql -c "update stage_prices set update = true where file_date is null and date != (select max(date) from stage_prices where file_date is null)"
+  psql -c "update stage_prices set update = false where file_date is null and update is null"
+  psql -c "update stage_prices set file_date='$d' where file_date is null"
+  psql -c "update stage_symbols set file_date='$d' where file_date is null"
+done
+
+psql -f conversions.sql
+psql -f create_prices.sql
