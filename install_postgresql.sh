79583a800c23acb2baae9987b373d4bdd4f9ca9c
Nick Bender <nbender@users.noreply.github.com>
2023-03-08 21:28:29 +0000

    Update install_postgresql.sh

--- install_postgresql.sh
+++ install_postgresql.sh
@@ -1,5 +1,6 @@
 sudo apt update
 sudo apt install postgresql postgresql-contrib
 sudo systemctl start postgresql.service
-#sudo -u postgres createuser --interactive
-#createdb ubuntu
+sudo -i -u postgres createuser ubuntu -d -r -s
+createdb ubuntu
+
